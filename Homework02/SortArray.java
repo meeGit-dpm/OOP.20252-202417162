9925d23602efa436997b755fe747dbf6d2142f17
meeGit-dpm <hangmai501h6@gmail.com>
2026-03-06 22:31:26 +0700

    Fixed

--- Homework02/SortArray.java
+++ Homework02/SortArray.java
@@ -1,22 +1,23 @@
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class SortArray {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> arr = new ArrayList<>();
+        String[] input = scanner.nextLine().split(" ");
 
-        System.out.println("Enter a list of number: ");
-        for(String s : scanner.nextLine().split(" ")){
-            arr.add(Integer.valueOf(s));
+        Integer[] arr = new Integer[input.length];
+
+        for(int i = 0; i < input.length; i++){
+            arr[i] = Integer.valueOf(input[i]);
         }
         
         sortArray(arr);
 
         System.out.println("Sorted array: ");
-        System.out.println(arr);
+        for(int i : arr){
+            System.out.print(i + " ");
+        }
+        System.out.println();
 
         System.out.println("Sum: " + sumArray(arr));
         System.out.println("Average: " + averageArray(arr));
@@ -24,11 +25,20 @@ public class SortArray {
 
     }
     
-    public static void sortArray(ArrayList<Integer> arr){
-        Collections.sort(arr);
+    public static void sortArray(Integer[] arr){
+        int s = arr.length;
+        for(int i = 0; i < s; i++){
+            for(int j = 0; j < s - i - 1; j++){
+                if(arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
     }
 
-    public static int sumArray(ArrayList<Integer> arr){
+    public static int sumArray(Integer[] arr){
         int sum = 0;
         for(int i : arr){
             sum += i;
@@ -36,8 +46,8 @@ public class SortArray {
         return sum;
     }
 
-    public static double averageArray(ArrayList<Integer> arr){
-        return (double)sumArray(arr)/(arr.size());
+    public static double averageArray(Integer[] arr){
+        return (double)sumArray(arr)/(arr.length);
     }
 
 }
